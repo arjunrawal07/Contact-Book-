d87ee34675173dea9442436a904a3fc2a26dd241
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-04-30 16:52:18 -0400

    got create working

--- lib/main.py
+++ lib/main.py
@@ -31,7 +31,29 @@ dolores = Entry(first_name='Dolores', last_name='Umbridge', address='1234 Owls L
                 phone_number='567-943-0943', birthday=date(1980, 4, 23))
 dolores.save()
 
-single_entry = input(
-    "To search for a single contact, enter the contact's first name: ")
-query = Entry.get(Entry.first_name == single_entry)
-print(f"{query.first_name} {query.last_name} {query.address} {query.phone_number} {query.birthday}")
+begin = input(
+    "What would you like to do? Find a contact or create a new entry? Find = Find, Create = Create ")
+
+if begin == "Find":
+    # Query by First Name
+    single_entry = input(
+        "To search for a single contact, enter the contact's first name: ")
+    single_query = Entry.get(Entry.first_name == single_entry)
+    print(f"{single_query.first_name} {single_query.last_name} {single_query.address} {single_query.phone_number} {single_query.birthday}")
+
+elif begin == "Create":
+    # Create a new Entry
+
+    create_first_name = input(
+        "Follow the prompts to create a new entry. First name: ")
+    create_last_name = input(
+        "Last name: "
+    )
+    create_address = input("Address: ")
+    create_phone_number = input("Phone Number (000-000-0000)")
+    create_birthday = input("Birthday (YYYY, MM, DD): ")
+
+    new_entry = Entry.create(first_name=create_first_name, last_name=create_last_name,
+                             address=create_address, phone_number=create_phone_number, birthday=create_birthday)
+    new_entry.save()
+    print(f"{new_entry.first_name} {new_entry.last_name} {new_entry.address} {new_entry.phone_number} {new_entry.birthday}")
